5a150033fafaa78f11c55c503ed77189f8db3e03
TanishqLambhate <tanulambhate@gmail.com>
2024-05-06 17:51:25 +0530

    sets

---
+++ day_9_sets.py
@@ -0,0 +1,13 @@
+# In sets we cannot access element using index because it has no order
+# unique,no order
+
+colors = {"red", "blue", "red", "green", "blue"}
+print(colors)
+print(set(colors))
+# using .add()
+colors.add("red")
+print(colors)
+
+unique_colors = set()
+unique_colors = {color for color in colors}
+print(unique_colors)
---
+++ day_9_tuple.py
@@ -0,0 +1,19 @@
+# Tuple
+
+# Tuple -Immutable
+person_list =["Sajana" ,"India",20]
+person =("Sanjana","India",20)
+
+print(person[0],person[2])
+
+#person_list[0]="Sneha"
+#person[0]="Sneha" #          ❌
+#Modification is not allowed
+#Remove -> .pop(), .remove()  ❌
+# Add -> .append() , .insert()❌
+# Methods
+print(person.count(20))  # 2
+print(person.index("India"))  # 1
+ 
+# index vs find
+# index - error if not found | find - -1  if not found
