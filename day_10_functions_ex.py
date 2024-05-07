89e62b7b837089c4448238d5ab96258078377da7
TanishqLambhate <tanulambhate@gmail.com>
2024-05-07 17:52:50 +0530

    function

---
+++ day_10_functions_ex.py
@@ -0,0 +1,161 @@
+library_list = [
+    {
+        "title": "Python Programming",
+        "author": "Eric Matthes",
+        "year": 2019,
+        "available": True
+    },
+    {
+        "title": "Automate the Boring Stuff with Python",
+        "author": "Al Sweigart",
+        "year": 2020,
+        "available": True
+    },
+    {
+        "title": "Learning Python I",
+        "author": "Mark Lutz",
+        "year": 2013,
+        "available": False
+    },
+    {
+        "title": "Fluent Python",
+        "author": "Luciano Ramalho",
+        "year": 2015,
+        "available": True
+    },
+    {
+        "title": "Adavance Python",
+        "author": "Mark Lutz",
+        "year": 2015,
+        "available": False
+    },
+]
+book = {"title": "Fluent Python II", "author": "Alex", "year": 2016, "available": True}
+# Task 1
+# Add Book Function: Write a function add_book(library, new_book)
+def add_book(library,new_book):
+    # library=[*library,new_book]
+    library.append(book)
+    
+    # print(library)
+
+add_book(library_list,book)
+# print(library_list)
+
+ # Task 2
+# Search Books by Author Function: Write a function search_by_author(library, author_name)
+# def search_book(library,author):
+#     author_books=[]
+#     for book1 in library:
+#         if author==book1.get("author"):
+#             author_books.append(book1)
+#     return author_books
+# print(search_book(library_list,"Mark Lutz"))
+
+#v2 pyhtonic way
+def search_book(library,author):
+    return [book1 for book1 in library if author==book1.get("author")]
+# print(search_book(library_list,"Mark Lutz"))
+
+# Task 3
+# Check Out Book Function: Write a function check_out_book(library, title) that marks a book as not available if it exists and is available in the library.
+# def check_out_book(library, title):
+#     found_flag=False
+#     for book1 in library:
+#         if title==book1.get("title"):
+#             if(book1.get("available")==True):
+#                 found_flag=True
+#                 return(f"{title} is Available for check out")
+#             elif(book1.get("available")==False):
+#                 found_flag=True
+#                 return("unavailable")
+#     if found_flag==False:
+#         return f"{title} is not preseent in library"
+
+def check_out_book(library, title):
+    for book1 in library:
+        if title==book1.get("title") and book1.get("available")==True:
+            book1["available"]=False
+            return(f"{title} is Available for check out")
+        elif title==book1.get("title") and book1.get("available")==False:
+            return("unavailable")
+    return f"{title} is not preseent in library"
+
+# print(check_out_book(library_list, "Fluent Python II"))
+# print(check_out_book(library_list, "Adavance Python"))
+# print(check_out_book(library_list, "Fluent Pn I"))
+
+movies = [
+    {
+        "title": "Inception",
+        "ratings": [5, 4, 5, 4, 5]
+    },
+    {
+        "title": "Interstellar",
+        "ratings": [5, 5, 4, 5, 4]
+    },
+    {
+        "title": "Dunkirk",
+        "ratings": [4, 4, 4, 3, 4]
+    },
+    {
+        "title": "The Dark Knight",
+        "ratings": [5, 5, 5, 5, 5]
+    },
+    {
+        "title": "Memento",
+        "ratings": [4, 5, 4, 5, 4]
+    },
+]
+# Function
+# Task 1
+# get_movies_avg_rating
+
+# Import package - Inbuilt
+
+from pprint import pprint
+from pprint import pprint
+#from package import function_name
+def get_movies_avg_rating(movies):
+    for movie in movies:
+        # movie["average_ratings"]=movie.get("average_ratings",0)
+        movie["average_ratings"]=sum(movie.get("ratings"))/len(movie.get("ratings"))
+    return movies    
+    # print (movies)
+    
+# pprint(get_movies_avg_rating(movies))
+# Task 2 - break it into 2 functions
+def calc_avg_ratings(movie):
+    return sum(movie.get("ratings"))/len(movie.get("ratings"))
+
+def get_movies_avg_rating(movies):
+    for movie in movies:
+        movie["average_ratings"]=sum(movie.get("ratings"))/len(movie.get("ratings"))
+    return movies 
+pprint(get_movies_avg_rating(movies))
+# Expected Output
+output = [
+    {"title": "Inception", "ratings": [5, 4, 5, 4, 5], "average_rating": 4.6},
+    {"title": "Interstellar", "ratings": [5, 5, 4, 5, 4], "average_rating": 4.2},
+    {"title": "Dunkirk", "ratings": [4, 4, 4, 3, 4], "average_rating": 4.1},
+    {"title": "The Dark Knight", "ratings": [5, 5, 5, 5, 5], "average_rating": 5},
+    {"title": "Memento", "ratings": [4, 5, 4, 5, 4], "average_rating": 4.2},
+]
+
+# def own_max(*nums):
+#     print(nums, type(nums))
+ 
+ 
+# own_max(5, 6, 10)
+# own_max(5, 6, 10, 7, 80, 60)
+# Arbitary Arguments
+def own_max(*nums):
+    curr_max=nums[0]
+    for n in nums:
+        if n>curr_max:
+            curr_max=n
+    return curr_max
+ 
+ 
+own_max(5, 6, 10)
+print(own_max(5, 6, 10, 7, 80, 60))
--- day_10_unpacking.py
+++ day_10_unpacking.py
@@ -3,35 +3,35 @@
 # b=10
 # c=10
 
-#Multiple variable assignment
-a=b=c=10
-print(a,b,c)
+# Multiple variable assignment
+a = b = c = 10
+print(a, b, c)
 
-#unpacking
-#poojitha,shivam,samar=("Black","Choco","Butterscotch")
-poojitha,shivam,samar="Black","Choco","Butterscotch" #Tuple without brackets
+# unpacking
+# poojitha,shivam,samar=("Black","Choco","Butterscotch")
+poojitha, shivam, samar = "Black", "Choco", "Butterscotch"  # Tuple without brackets
 print(poojitha)
 print(shivam)
 print(samar)
 
-movies=["MI" ,"JJK" ,"Avengers"]
-mathesh,nandini,devesh=movies
-print(mathesh,nandini,devesh)
+movies = ["MI", "JJK", "Avengers"]
+mathesh, nandini, devesh = movies
+print(mathesh, nandini, devesh)
 
 # t1,t2,t3=[100,200,300,400]
-# print(t1,t2,t3) #ValueError: too many values to unpack (expected 3) 
+# print(t1,t2,t3) #ValueError: too many values to unpack (expected 3)
 
-t1,t2,t3,_=[100,200,300,400]
-print(t1,t2,t3)
+t1, t2, t3, _ = [100, 200, 300, 400]
+print(t1, t2, t3)
 
 # *-> unpacking operator
-t1,t2,*t3=[100,200,300,400,60,30]
-print(t1,t2,t3)# 100 200 [300, 400, 60, 30]
-t1,t2,*t3=(100,200,300,400,60,30)
-print(t1,t2,t3)# 100 200 [300, 400, 60, 30]
+t1, t2, *t3 = [100, 200, 300, 400, 60, 30]
+print(t1, t2, t3)  # 100 200 [300, 400, 60, 30]
+t1, t2, *t3 = (100, 200, 300, 400, 60, 30)
+print(t1, t2, t3)  # 100 200 [300, 400, 60, 30]
 
 # Task 1
-coordinates=[(5,4),(1,1),(6,10),(9,10)]
+coordinates = [(5, 4), (1, 1), (6, 10), (9, 10)]
 # for i in coordinates:
 #     # print(i)
 #     for j in i:
@@ -55,11 +55,37 @@ coordinates=[(5,4),(1,1),(6,10),(9,10)]
 # print(distance)
 
 # distance=[]
-# for x,y in coordinates:  
+# for x,y in coordinates:
 #     d=(x**2+y**2)**0.5
 #     distance.append(d)
 # print(distance)
 
 # Task 1.3 - unpacking + list comprehension
-distance=[round((x**2+y**2)**0.5,2) for x,y in coordinates]
+distance = [round((x**2 + y**2) ** 0.5, 2) for x, y in coordinates]
 print(distance)
+
+marks1 = [70, 80, 60]
+# marks2=[*marks1] # copy -1. slice 2. .copy()
+marks2 = [*marks1, 75, 68]
+print(marks2)
+
+# merging multiple list
+t1 = [80, 90]
+t2 = [50, 60]
+t3 = [*t1, *t2]
+print(t3)
+# t3=[80,90,50,60]
+# Unpacking -> ** Dict
+movie = {"name": "John wick", "year": 2014}
+# Copy
+mv1 = {**movie, "actor": "keanu Reeves"}
+mv2 = {**movie, "actor": "keanu Reeves", "year": 2015}
+mv3 = {
+    "actor": "keanu Reeves",
+    "year": 2015,
+    **movie,
+}
+print(mv1)
+print(mv2)
+print(mv3)
+print(movie)
